121da89c033372b9c08ca704586880938f16f21c
Yulianna <r.yulianna777@gmail.com>
2023-07-07 06:12:20 +0300

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pacman = document.querySelector(".pacman");
-let speed = 5;
+let speed = 8;
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 
@@ -11,21 +11,29 @@ window.addEventListener("load", () => {
 
 window.addEventListener("keyup", (e) => {
     switch (e.key) {
-        case "ArrowLeft":
-            pacman.style.left = parseInt(pacman.style.left) - speed + "px";
-            pacman.style.transform = 'rotate(180deg)';
-            break;
-        case "ArrowRight":
-            pacman.style.left = parseInt(pacman.style.left) + speed + "px";
-            pacman.style.transform = 'rotate(0deg)';
-            break;
-        case "ArrowUp":
-            pacman.style.top = parseInt(pacman.style.top) - speed + "px";
-            pacman.style.transform = 'rotate(270deg)';
-            break;
-        case "ArrowDown":
+    case "ArrowLeft":
+        if (pacman.style.left > 0 + "px") {
+        pacman.style.left = parseInt(pacman.style.left) - speed + "px";
+        pacman.style.transform = "rotate(180deg)";
+        }
+        break;
+    case "ArrowRight":
+        if(pacman.style.left < 226 + "px"){
+        pacman.style.left = parseInt(pacman.style.left) + speed + "px";
+        pacman.style.transform = "rotate(0deg)";
+        }
+        break;
+    case "ArrowUp":
+        if (pacman.style.top > 0 + "px") {
+        pacman.style.top = parseInt(pacman.style.top) - speed + "px";
+        pacman.style.transform = "rotate(270deg)";
+        }
+        break;
+    case "ArrowDown":
+        if(pacman.style.top < 248 + "px" ){
             pacman.style.top = parseInt(pacman.style.top) + speed + "px";
-            pacman.style.transform = 'rotate(90deg)';
-            break;
+            pacman.style.transform = "rotate(90deg)";
+        }
+        break;
     }
 });
